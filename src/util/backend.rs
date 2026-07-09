a84fe37cb00d402f7c2f8b2e8bc85fc3166be6ea
Mart van Buren <mart@fractic.io>
2026-07-09 15:40:14 +0900

    Update AWS behaviour version to always point to latest.

--- src/util/backend.rs
+++ src/util/backend.rs
@@ -223,7 +223,7 @@ register_ctx_singleton!(
     dyn S3Backend,
     |ctx: Arc<dyn S3CtxView>| async move {
         let region = Region::new(ctx.s3_region().clone());
-        let shared_config = aws_config::defaults(BehaviorVersion::v2026_01_12())
+        let shared_config = aws_config::defaults(BehaviorVersion::latest())
             .region(region)
             .load()
             .await;
